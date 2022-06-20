009697937f49bd1fdaaf6fcdf436aaa9eb25573b
vahid <yaftarpjohan@gmail.com>
2022-06-20 14:27:37 +0430

    fix some bug static root and language and time

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'UK'
+LANGUAGE_CODE = 'EN'
 
 TIME_ZONE = 'Asia/Tehran'
 
